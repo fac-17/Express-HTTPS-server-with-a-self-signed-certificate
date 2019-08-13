5d398153e30404660db4c639839ddae2ed5a32db
gminova <minova1995@gmail.com>
2019-08-13 15:57:47 +0100

    certificate and key added, server updated

--- server.js
+++ server.js
@@ -1,10 +1,20 @@
-const https = require('https')
-const app = express()
+const https = require("https");
+const express = require("express");
+const app = express();
+const fs = require("fs");
+const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello HTTPS!')
-})
+app.get("/", (req, res) => {
+  res.send("Hello HTTPS!");
+});
 
-https.createServer({}, app).listen(3000, () => {
-  console.log('Listening...')
-})
+https.createServer(
+    {
+      key: fs.readFileSync("server.key"),
+      cert: fs.readFileSync("server.cert")
+    },
+    app
+  )
+  .listen(port, () => {
+    console.log(`Example app listening on port ${port}!`);
+  });
